272315640ac1e5282a3ccc3ef0cc898ace77907c
Avery VanKirk <fredknex@gmail.com>
2017-11-11 17:45:16 -0800

    added optional command line arg to override default fizzbuzz range

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,10 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
+
+	var fizzRange = 50
+
+	if len(os.Args) > 1 {
+
+		r,err := strconv.Atoi(os.Args[1])
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fizzRange = r
+	}
 	
-	for i:=0;i<50;i++{
+	for  i:= 0; i < fizzRange ; i++ {
 		if i % 3 == 0 && i % 5 == 0 {
 			fmt.Println("fizzbuzz")
 		} else if i % 3 == 0 {
